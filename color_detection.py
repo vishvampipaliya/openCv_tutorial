6de19574042d2ee74169bc90a0579d8ca2571582
vishvampipaliya <vishvam028@gmail.com>
2026-07-20 19:25:48 +0530

    color detection

---
+++ color_detection.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # cv2.imshow('hsv', hsv)
+    lower_blue = np.array([90, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', res)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
